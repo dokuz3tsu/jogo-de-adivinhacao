22921c47ed29789916095899ed4f553100076615
dokuz3tsu <mateus.wieira@escola.pr.gov.br>
2025-02-18 08:42:06 -0300

    JOGO

---
+++ jogo de adivinhaçao.py
@@ -0,0 +1,13 @@
+print("******************************")
+print("Bem vindo ao jogo de adivinhaçao")
+print("******************************")
+
+numero secreto=34
+
+chute_str = input("digite o seu numero")
+print("Voce digitou", chute_srt)
+chute = int(chute_srt)
+print("Voce acertou")
+else:
+     print("Voce errou")
+      print("Fim de jogo!")
